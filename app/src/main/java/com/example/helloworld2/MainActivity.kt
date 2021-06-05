2acdc9890b6ba0feab6f3b21c46496977d418001
hunterachieng <achienghunter@gmail.com>
2021-06-05 09:53:02 +0300

    Registration

--- app/src/main/java/com/example/helloworld2/MainActivity.kt
+++ app/src/main/java/com/example/helloworld2/MainActivity.kt
@@ -3,20 +3,18 @@ package com.example.helloworld2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     //casting:allows you to use variables in other functions
     //Used when not immediately passing the values
     lateinit var etName: EditText
     lateinit var etDob: EditText
-    lateinit var etNationality: EditText
     lateinit var etIDNumber: EditText
     lateinit var   etEmail:EditText
     lateinit var  etPhoneNumber:EditText
     lateinit var btnRegister:Button
+    lateinit var spNationality:Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +26,15 @@ class MainActivity : AppCompatActivity() {
     fun castViews() {
         etName = findViewById(R.id.etName)
         etDob = findViewById(R.id.etDob)
-        etNationality = findViewById(R.id.etNationality)
         etIDNumber = findViewById(R.id.etIDNumber)
         etEmail = findViewById(R.id.etEmail)
         etPhoneNumber = findViewById(R.id.etPhoneNumber)
         btnRegister = findViewById(R.id.btnRegisterLbl)
-
+        spNationality = findViewById(R.id.spNationality)
+        val nationalities = arrayOf("Kenyan", "Rwandan", "South Sudanese", "Ugandan")
+        var adapter = ArrayAdapter<String>(baseContext,android.R.layout.simple_spinner_item,nationalities)
+         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spNationality.adapter=adapter
     }
 
     fun clickRegister() {
@@ -46,7 +47,7 @@ class MainActivity : AppCompatActivity() {
             if (dob.isEmpty()) {
                 etDob.setError("Dob is required")
             }
-            var nationality = etNationality.text.toString()
+            var nationality = spNationality.selectedItem.toString()
             var idNumber = etIDNumber.text.toString()
             var email = etEmail.text.toString()
             var phoneNumber = etPhoneNumber.text.toString()
